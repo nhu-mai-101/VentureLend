0d505efcc2441753e804162c416a4e42bd133b71
Nhu Mai <nhumaix3@gmail.com>
2021-12-01 17:29:21 -0800

    Add material UI global theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './ThemeContext.tsx'
 import Header from './components/Header.tsx';
 import LandingPage from './pages/Landing.tsx';
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <LandingPage />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <LandingPage />
+      </ThemeProvider>
     </div>
   )
 }
