2753419ca8b495a6e8a196c3a804334bbdd07a15
Nhu Mai <nhumaix3@gmail.com>
2021-12-02 01:06:07 -0800

    Added info cards and buttons for borrowing/investing

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import BorrowInvestCards from '../components/BorrowInvestCards.tsx';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Background>
+        <ActionButton variant="contained">Borrow</ActionButton>
+        <ActionButton variant="contained">Invest</ActionButton>
+      </Background>
+      <BorrowInvestCards />
+    </div>
+  )
+}
+
+export default LandingPage;
 
 const Background = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: end;
   width: 100vw;
   height: 60vh;
   background-image: url('https://content1.getnarrativeapp.com/static/453cc1b7-7a5d-45ad-873f-aaa41845d566/family-in-home-connecticut-candid.jpg');
@@ -9,13 +30,9 @@ const Background = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
 `
-
-const LandingPage = () => {
-  return (
-    <div>
-      <Background />
-    </div>
-  )
-}
-
-export default LandingPage;
+const ActionButton = styled(Button)`
+  width: 200px;
+  height: 65px;
+  margin: 100px 20px;
+  font-size: 25px;
+`;
