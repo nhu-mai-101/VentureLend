bb7e8954c3869343bf87829648306dd81d5df264
Nhu Mai <nhumaix3@gmail.com>
2021-12-02 15:23:33 -0800

    Create sign up form with MUI components

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,10 +1,62 @@
 import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const SignUp = () => {
   return (
-    <div>
-      Sign Up
-    </div>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '90%' },
+        width: '30vw',
+        border: '1px solid grey',
+        borderRadius: 2,
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 'auto'
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+        Sign Up
+      </Typography>
+      <TextField
+        required
+        id="firstName"
+        label="First Name"
+        variant="outlined"
+      />
+      <TextField
+        required
+        id="lastName"
+        label="Last Name"
+        variant="outlined"
+      />
+      <TextField
+        required
+        id="email"
+        label="E-mail"
+        variant="outlined"
+      />
+      <TextField
+        required
+        id="password"
+        label="Password"
+        variant="outlined"
+      />
+      <Button color="inherit">
+        SIGN UP
+      </Button>
+      <Typography>
+        Already have an account? Log In
+      </Typography>
+    </Box>
   )
 }
 
