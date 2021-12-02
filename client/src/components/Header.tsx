01b3e7893895c2686973d077898b4f548b0f5742
Nhu Mai <nhumaix3@gmail.com>
2021-12-02 01:05:41 -0800

    Link login/signup buttons with react-router

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import * as React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignUp from '../pages/SignUp.tsx';
+import LogIn from '../pages/LogIn.tsx';
+import styled from 'styled-components';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,16 +13,43 @@ const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <StyledToolbar>
           <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
             LendingClub
           </Typography>
-          <Button color="inherit">Sign Up</Button>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+
+          <Router>
+            <Link to="/signup">
+              <ActionButton color="inherit">Sign Up</ActionButton>
+            </Link>
+            <Link to="/login">
+              <ActionButton color="inherit">Login</ActionButton>
+            </Link>
+            <Routes>
+              <Route path="/signup">
+                <SignUp />
+              </Route>
+              <Route path="/login">
+                <LogIn />
+              </Route>
+            </Routes>
+          </Router>
+
+        </StyledToolbar>
       </AppBar>
     </Box>
   )
 }
 
 export default Header;
+
+const StyledToolbar = styled(Toolbar)`
+  height: 80px;
+`;
+
+const ActionButton = styled(Button)`
+  width: 150px;
+  height: 40px;
+  margin: 5px;
+  font-size: 20px;
+`;
