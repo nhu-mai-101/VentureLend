4453a10de8684b1efb63250293b2fb958354493c
Nhu Mai <nhumaix3@gmail.com>
2021-12-04 04:36:29 -0800

    getUser info route

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useAuth} from '../../contexts/AuthContext';
+import axios from 'axios';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import LoanList from './LoanList'
@@ -7,11 +8,27 @@ import LoanList from './LoanList'
 
 const UserProfile = () => {
   const { currentUser } = useAuth();
-  console.log(currentUser.email)
+  const [user, setUser] = useState<string>('')
+
+  const getUserInfo = (userEmail: string) => {
+    axios.get('http://localhost:3000/getUser', {
+      params: {
+        email: userEmail
+      }
+    })
+    .then((data) => {
+      // setUser(data);
+      console.log('success meow', data);
+    })
+    .catch((error) => {console.log('oh no catch', error)})
+  }
+
+  useEffect(() => {getUserInfo(currentUser.email)}, [])
+
   return (
     <div>
       <div>
-        Hello USER NAME <br />
+        Hello {user} <br />
         E-mail: {currentUser.email}
       </div>
       <div>
