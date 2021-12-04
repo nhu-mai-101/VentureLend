731ab58ee69517e8f1d8d6f965fcacd9a1b0904c
Nhu Mai <nhumaix3@gmail.com>
2021-12-03 23:30:14 -0800

    Create db schema

---
+++ schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS lending;
+
+CREATE DATABASE lending;
+
+USE lending;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(25) NOT NULL,
+  last_name VARCHAR(25) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE loans (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  total DECIMAL(8,2) NOT NULL,
+  paid DECIMAL(8,2),
+  oustanding DECIMAL(8,2),
+  apr DECIMAL (4,3) NOT NULL,
+  term INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE user_loans (
+  user_id INT NOT NULL,
+  loan_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (loan_id) REFERENCES loans(id)
+);
+
+CREATE TABLE user_investments (
+  user_id INT NOT NULL,
+  loan_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (loan_id) REFERENCES loans(id)
+);
