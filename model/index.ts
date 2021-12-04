c835eaa6d3570769c2cc8a59a589b24545be2c52
Nhu Mai <nhumaix3@gmail.com>
2021-12-03 23:33:45 -0800

    Define model functions for routes

--- model/index.ts
+++ model/index.ts
@@ -1,1 +1,66 @@
-export {}
+import {db} from '../db/index.js';
+import {User} from '../types/users';
+import {Loans} from '../types/loans'
+
+export const model = {
+  newUser: (callback: Function, body: User) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  currentLoans: (callback: Function) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  newLoan: (callback: Function, body: Loans) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  newInvestment: (callback: Function, body: Loans) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  currentInvestments: (callback: Function) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  availableInvestments: (callback: Function) => {
+    let qString = '';
+    db.query(qString, (error: Error, data) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+}
