5947a21a3ba996a26a96ffd5216d33a8933372cf
Nhu Mai <nhumaix3@gmail.com>
2021-12-03 23:29:58 -0800

    Create mysql db connection

--- db/index.ts
+++ db/index.ts
@@ -1,1 +1,15 @@
-export {}
+import mysql from 'mysql';
+
+export const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'lending'
+});
+
+db.connect((error, data) => {
+  if (error) {
+    console.log('error connecting to db: ', error)
+  } else {
+    console.log ('success connecting to db')
+  }
+});
