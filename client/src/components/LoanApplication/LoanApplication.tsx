2a3f880ec17884d7bac58a620208061e99be0869
Nhu Mai <nhumaix3@gmail.com>
2021-12-03 21:54:00 -0800

    Skeleton loan app page/form

--- client/src/components/LoanApplication/LoanApplication.tsx
+++ client/src/components/LoanApplication/LoanApplication.tsx
@@ -1,1 +1,13 @@
-export {}
+import * as React from 'react';
+import LoanAppForm from './LoanAppForm'
+
+const LoanApplication = () => {
+  return (
+    <div>
+      Loan Application:
+      <LoanAppForm />
+    </div>
+  )
+}
+
+export default LoanApplication;
