64f6b19ccbb9e5aecdb915092b7fb2c4c75c3d23
Nhu Mai <nhumaix3@gmail.com>
2021-12-04 19:42:32 -0800

    Completed route to get user info

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -6,29 +6,60 @@ import { Link } from "react-router-dom";
 import LoanList from './LoanList'
 
 
+interface IUserInfo {
+  userId: Number;
+  firstName: String;
+  lastName: String;
+  email: String
+}
+
 const UserProfile = () => {
   const { currentUser } = useAuth();
-  const [user, setUser] = useState<string>('')
+  const [userInfo, setUserInfo] = useState<Partial<IUserInfo>>({
+    userId: undefined,
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+  // const [currentLoans, setCurrentLoans] = useState<any>();
 
-  const getUserInfo = (userEmail: string) => {
-    axios.get('http://localhost:3000/getUser', {
-      params: {
-        email: userEmail
-      }
-    })
-    .then((data) => {
-      // setUser(data);
-      console.log('success meow', data);
-    })
-    .catch((error) => {console.log('oh no catch', error)})
+
+  const getUserInfo = async (userEmail: string) => {
+    try {
+      await axios.get('http://localhost:3000/getUser', {
+        params: {
+          email: userEmail
+        }
+      })
+      .then((data) => {
+        setUserInfo({
+          ...userInfo,
+          userId: data.data[0].id,
+          firstName: data.data[0].first_name,
+          lastName: data.data[0].last_name,
+          email: data.data[0].email
+        });
+      });
+      // await axios.get('http://localhost:3000/currentLoans', {
+      //   params: {
+      //     userId: userInfo.userId
+      //   }
+      // })
+      // .then((data) => {
+      //   setCurrentLoans(data)
+      // })
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {getUserInfo(currentUser.email)}, [])
 
   return (
     <div>
+      {/* {console.log('currentLoans:', currentLoans)} */}
       <div>
-        Hello {user} <br />
+        Hello {userInfo.firstName}<br />
         E-mail: {currentUser.email}
       </div>
       <div>
