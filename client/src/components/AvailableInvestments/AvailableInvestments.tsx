156a50e467a4e6f9f3e025516a2b3210b7d05ae4
Nhu Mai <nhumaix3@gmail.com>
2021-12-07 05:01:26 -0800

    Render current investments onto profile, render all uninvested loans to make a new investment

--- client/src/components/AvailableInvestments/AvailableInvestments.tsx
+++ client/src/components/AvailableInvestments/AvailableInvestments.tsx
@@ -1,15 +1,19 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import  { useUser } from '../UserProfile/UserProfile';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const style = {
   position: 'absolute' as 'absolute',
   top: '50%',
   left: '50%',
+  width: '60%',
+  length: '80%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -18,8 +22,54 @@ const style = {
 
 const AvailableInvestments = () => {
   const [open, setOpen] = React.useState(false);
+  const [availableInvestments, setAvailableInvestments] = React.useState<any>();
+  const [selectedInvestments, setSelectedInvestments] = React.useState<number>(0);
+  const { userId } = useUser();
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const columns: GridColDef[] = [
+    { field: 'total', headerName: 'Total', type: 'number', width: 150 },
+    { field: 'paid', headerName: 'Amount Paid', type: 'number', width: 150 },
+    { field: 'oustanding', headerName: 'Amount Outstanding', type: 'number', width: 150 },
+    { field: 'apr', headerName: 'APR', type: 'number', width: 150 },
+    { field: 'term', headerName: 'Loan Term', type: 'number', width: 150 },
+  ];
+
+  const getAvailableInvestments = async () => {
+    try {
+      await axios.get('http://localhost:3000/availableInvestments', {
+        params: {
+          userId: userId
+        }
+      })
+      .then((data) => {
+        setAvailableInvestments(data.data)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSelection = (e: any) => {
+    setSelectedInvestments(e - 1)
+  }
+
+  const newInvestment = async () => {
+    try {
+      await axios.put('http://localhost:3000/newInvestment', {
+        loanId: availableInvestments[selectedInvestments].id,
+        userId: userId
+      })
+      handleClose();
+      getAvailableInvestments();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {getAvailableInvestments()}, [open]);
 
   return (
     <div>
@@ -32,11 +82,18 @@ const AvailableInvestments = () => {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            Available Loans
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <DataGrid
+            rows={availableInvestments}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            autoHeight={true}
+            onSelectionModelChange={handleSelection}
+          />
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={newInvestment}>Submit</Button>
         </Box>
       </Modal>
     </div>
---
+++ client/src/components/AvailableInvestments/CurrentInvestmentsList.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+type Props = {
+  loans: any;
+}
+
+const CurrentInvestmentsList = ({loans}: Props) => {
+  const columns: GridColDef[] = [
+    { field: 'total', headerName: 'Total', type: 'number', width: 150 },
+    { field: 'paid', headerName: 'Amount Paid', type: 'number', width: 150 },
+    { field: 'oustanding', headerName: 'Amount Outstanding', type: 'number', width: 150 },
+    { field: 'apr', headerName: 'APR', type: 'number', width: 150 },
+    { field: 'term', headerName: 'Loan Term', type: 'number', width: 150 },
+  ];
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={loans}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        autoHeight={true}
+        disableSelectionOnClick={true}
+      />
+    </div>
+  )
+}
+
+export default CurrentInvestmentsList;
