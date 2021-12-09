4af98153e9e02eac49816a1af5a0181c77d59613
Nhu Mai <nhumaix3@gmail.com>
2021-12-09 05:14:07 -0800

    Style user profile components

--- client/src/components/UserProfile/LoanList.tsx
+++ client/src/components/UserProfile/LoanList.tsx
@@ -8,10 +8,10 @@ type Props = {
 const LoanList = ({loans}: Props) => {
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', type: 'number', width: 100 },
-    { field: 'total', headerName: 'Total', type: 'number', width: 150 },
-    { field: 'paid', headerName: 'Amount Paid', type: 'number', width: 150 },
-    { field: 'oustanding', headerName: 'Amount Outstanding', type: 'number', width: 150 },
-    { field: 'apr', headerName: 'APR', type: 'number', width: 150 },
+    { field: 'total', headerName: 'Total', type: 'number', width: 100 },
+    { field: 'paid', headerName: 'Paid', type: 'number', width: 100 },
+    { field: 'oustanding', headerName: 'Outstanding', type: 'number', width: 125 },
+    { field: 'apr', headerName: 'APR', type: 'number', width: 100 },
     { field: 'term', headerName: 'Loan Term', type: 'number', width: 150 },
   ];
 
@@ -20,9 +20,8 @@ const LoanList = ({loans}: Props) => {
       <DataGrid
         rows={loans}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        autoHeight={true}
+        pageSize={10}
+        rowsPerPageOptions={[10, 50, 100]}
         disableSelectionOnClick={true}
       />
     </div>
