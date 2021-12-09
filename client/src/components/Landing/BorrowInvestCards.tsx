159fd651a7346c895e3eb0a931affa4fe4746c2a
Nhu Mai <nhumaix3@gmail.com>
2021-12-09 02:22:47 -0800

    Style borrow and invest step cards

--- client/src/components/Landing/BorrowInvestCards.tsx
+++ client/src/components/Landing/BorrowInvestCards.tsx
@@ -1,55 +1,68 @@
 import styled from 'styled-components';
 import Card from '@mui/material/Card';
 import { CardContent, Typography } from '@mui/material';
+import { LooksOne, LooksTwo, Looks3 } from '@mui/icons-material';
 
 const BorrowInvestCards = () => {
   return (
     <BorrowInvestContainer>
-      <BorrowerContainer>
-        <BorrowerCard>
-          <CardContent>
-            <Typography>
-              Borrow Step 1:
-            </Typography>
-          </CardContent>
-        </BorrowerCard>
-        <BorrowerCard>
-          <CardContent>
-            <Typography>
-              Step 2:
-            </Typography>
-          </CardContent>
-        </BorrowerCard>
-        <BorrowerCard>
-          <CardContent>
-            <Typography>
-              Step 3:
-            </Typography>
-          </CardContent>
-        </BorrowerCard>
+      <BorrowerContainer id='borrow'>
+        <Typography variant='h3' color='white'>Need a loan? We got your back.</Typography>
+        <CardContainer>
+          <BorrowerCard>
+            <CardContent>
+              <LooksOne sx={{fontSize: '90px', opacity: 0.7, }}/>
+              <StepText>
+                Vestibulum cursus ligula et pulvinar pellentesque. Nunc et nunc lacus.
+              </StepText>
+            </CardContent>
+          </BorrowerCard>
+          <BorrowerCard>
+            <CardContent>
+              <LooksTwo sx={{fontSize: '90px', opacity: 0.7}}/>
+              <StepText>
+                Mauris sem ex, volutpat a nulla eu, bibendum interdum libero turpis a.
+              </StepText>
+            </CardContent>
+          </BorrowerCard>
+          <BorrowerCard>
+            <CardContent>
+              <Looks3 sx={{fontSize: '90px', opacity: 0.7}}/>
+              <StepText>
+                Ut in risus interdum, tristique augue sem, nulla turpis pulvinar nunc.
+              </StepText>
+            </CardContent>
+          </BorrowerCard>
+        </CardContainer>
       </BorrowerContainer>
-      <InvestorContainer>
-        <InvestorCard>
-          <CardContent>
-            <Typography>
-              Invest Step 1:
-            </Typography>
-          </CardContent>
-        </InvestorCard>
-        <InvestorCard>
-          <CardContent>
-            <Typography>
-              Step 2:
-            </Typography>
-          </CardContent>
-        </InvestorCard>
-        <InvestorCard>
-          <CardContent>
-            <Typography>
-              Step 3:
-            </Typography>
-          </CardContent>
-        </InvestorCard>
+      <InvestorContainer id='invest'>
+        <Typography variant='h3' color='primary'>Want to invest? We've got great returns.</Typography>
+        <CardContainer>
+          <InvestorCard>
+            <CardContent>
+              <LooksOne sx={{fontSize: '90px', opacity: 0.7, }}/>
+              <StepText>
+                Maecenas odio sem, imperdiet non viverra sed, venenatis sed massa.
+              </StepText>
+            </CardContent>
+          </InvestorCard>
+          <InvestorCard>
+            <CardContent>
+              <LooksTwo sx={{fontSize: '90px', opacity: 0.7}}/>
+              <StepText>
+                Pellentesque hendrerit nibh blandit, venenatis eros eget, vehicula nisl.
+              </StepText>
+            </CardContent>
+          </InvestorCard>
+          <InvestorCard>
+            <CardContent>
+              <Looks3 sx={{fontSize: '90px', opacity: 0.7}}/>
+              <StepText>
+                Etiam laoreet, tellus eget lacinia hendrerit, massa enim semper.
+              </StepText>
+            </CardContent>
+          </InvestorCard>
+        </CardContainer>
       </InvestorContainer>
     </BorrowInvestContainer>
   )
@@ -63,35 +76,63 @@ const BorrowInvestContainer = styled.div`
 
 const BorrowerContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
-  padding: 50px;
+  padding: 25px;
+  margin-top: 50px;
+  margin-bottom: 50px;
   background-color: #287066AA;
 `;
 
 const BorrowerCard = styled(Card)`
-  width: 30vw;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 25vw;
   height: 400px;
   font-size: 30px;
   margin: auto;
   background-color: #FFFFFFAA;
+  justify-content: center;
+  align-items: center;
 `;
 
 const InvestorContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
-  padding: 50px;
+  padding: 25px;
+  margin-top: 50px;
+  margin-bottom: 50px;
 `;
 
 const InvestorCard = styled(Card)`
-  width: 30vw;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 25vw;
   height: 400px;
   font-size: 30px;
   margin: auto;
   background-color: #287066AA;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StepText = styled(Typography)`
+  font-size: 30px;
+  text-align: center;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 80vw;
+  margin: 30px;
 `;
