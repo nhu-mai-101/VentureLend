d48512e0319bf39f1cc046fe69db9db8661e5ef3
Nhu Mai <nhumaix3@gmail.com>
2021-12-08 23:58:49 -0800

    Styling

--- client/src/components/App/Header.tsx
+++ client/src/components/App/Header.tsx
@@ -1,62 +1,61 @@
-import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {AppBar, Box, Toolbar, Typography, Button, Avatar} from '@mui/material';
+import styled from 'styled-components';
 import SignUp from '../SignUp/SignUp';
 import LogIn from '../LogIn/LogIn';
 import LandingPage from '../Landing/Landing';
 import UserProfile from '../UserProfile/UserProfile';
-import LoanApplication from '../LoanApplication/LoanApplication'
-import styled from 'styled-components';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
+import Footer from './Footer';
 
 const Header = () => {
   return (
-    <Router>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <StyledToolbar >
-            <Avatar component={Link} to={'/'}>LC</Avatar>
-            <Typography variant="h3" sx={{ flexGrow: 1 }}>
-              LendingClub
-            </Typography>
-            <Button color="inherit" component={Link} to={'/signup'}>Sign Up</Button>
-            <Button color="inherit" component={Link} to={'/login'}>Login</Button>
-          </StyledToolbar>
-        </AppBar>
-          <Routes>
-            <Route path="/" element={<LandingPage />}>
-            </Route>
-            <Route path="/signup" element={<SignUp />}>
-            </Route>
-            <Route path="/login" element={<LogIn />}>
-            </Route>
-            <Route path="/userprofile" element={<UserProfile />}>
-            </Route>
-            <Route path="/loanapp" element={<LoanApplication />}>
-            </Route>
-            <Route path="/invest" element={<LoanApplication />}>
-            </Route>
-          </Routes>
-      </Box>
-    </Router>
+    <header>
+      <Router>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position='static'>
+            <StyledToolbar >
+              <Avatar component={Link} to={'/'}>LC</Avatar>
+              <Typography variant='h2' sx={{ flexGrow: 1, fontWeight: 'medium', letterSpacing: 4 }}>
+                LendingClub
+              </Typography>
+              <NavLinks to='/'>About Us</NavLinks>
+              <NavLinks to='/'>Our Services</NavLinks>
+              <NavLinks to='/'>FAQ</NavLinks>
+              <Button sx={{color:"inherit", fontSize: 20, border: '1px solid white', margin: '10px', width: '150px', height: '45px'}} component={Link} to={'/signup'}>Sign Up</Button>
+              <Button sx={{color:"inherit", fontSize: 20, border:'1px solid white', margin: '10px', width: '150px', height: '45px'}} component={Link} to={'/login'}>Login</Button>
+            </StyledToolbar>
+          </AppBar>
+        </Box>
+        <Routes>
+          <Route path="/" element={<LandingPage />}>
+          </Route>
+          <Route path="/signup" element={<SignUp />}>
+          </Route>
+          <Route path="/login" element={<LogIn />}>
+          </Route>
+          <Route path="/userprofile" element={<UserProfile />}>
+          </Route>
+        </Routes>
+      </Router>
+      <Footer />
+    </header>
   )
 }
 
 export default Header;
 
 const StyledToolbar = styled(Toolbar)`
-  height: 80px;
+  height: 100px;
   display: flex;
   flex-direction: row;
 `;
 
-const ActionButton = styled(Button)`
-  width: 150px;
-  height: 40px;
-  margin: 5px;
-  font-size: 20px;
+const NavLinks = styled(Link)`
+  margin: 20px;
+  font-size: 25px;
+  color: white;
+  text-decoration: none;
+  &:focus, &:hover, &:visited, &:link, &:active {
+    text-decoration: none;
+  };
 `;
--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import Button from '@mui/material/Button';
+import { Button, Typography } from '@mui/material';
 import BorrowInvestCards from './BorrowInvestCards';
 
 
@@ -11,6 +11,11 @@ const LandingPage = () => {
         <ActionButton variant="contained">Borrow</ActionButton>
         <ActionButton variant="contained">Invest</ActionButton>
       </Background>
+      <div>
+        <Typography variant='h3' sx={{fontStyle: 'oblique', fontWeight: 'bold', letterSpacing: 6, textAlign: 'center', padding: 10}}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel tellus id nibh imperdiet mollis ut sit amet ante."
+        </Typography>
+      </div>
       <BorrowInvestCards />
     </div>
   )
