d81898afc2f2418f3dd95ffcd978a4964ecfc2d6
Nhu Mai <nhumaix3@gmail.com>
2021-12-09 20:52:37 -0800

    Clean up MUI import statements

--- client/src/components/SignUp/SignUpForm.tsx
+++ client/src/components/SignUp/SignUpForm.tsx
@@ -1,13 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import {useState, useRef}  from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import {useAuth} from '../../contexts/AuthContext';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
+import { useAuth } from '../../contexts/AuthContext';
+import { Button, TextField, Box, Typography, Alert } from '@mui/material';
 
 const SignUpForm = () => {
   const firstNameRef = useRef<HTMLInputElement>(null!);
